2d46db0d01d757f4e6cb99644cb86899be75fe92
jinkook0126 <jinqook0126@naver.com>
2025-09-23 13:59:53 +0900

    feat: remix 패키지 버전 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
   plugins: [
@@ -8,7 +15,10 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
       },
     }),
+    tsconfigPaths(),
   ],
 });
